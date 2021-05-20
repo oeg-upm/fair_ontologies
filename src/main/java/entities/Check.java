ad652df46c595c5b5ff6fba3c63232954bc00929
Daniel Garijo <dgarijov@gmail.com>
2021-05-20 10:45:13 +0200

    Updated licenses

--- src/main/java/entities/Check.java
+++ src/main/java/entities/Check.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2013 Ontology Engineering Group, Universidad Politecnica de Madrid, Spain
+ * Copyright 2021-22 Ontology Engineering Group, Universidad Politecnica de Madrid, Spain
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+
 package entities;
 
 import com.google.gson.annotations.Expose;
--- src/main/java/entities/Ontology.java
+++ src/main/java/entities/Ontology.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021-22 Ontology Engineering Group, Universidad Politecnica de Madrid, Spain
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package entities;
 
 import fair.Constants;
@@ -5,13 +21,11 @@ import fair.Utils;
 import org.jsoup.nodes.Document;
 import org.semanticweb.owlapi.model.OWLAnnotation;
 import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.rdf.rdfxml.renderer.OWLOntologyXMLNamespaceManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Ontology {
     private OWLOntology ontologyModel;
