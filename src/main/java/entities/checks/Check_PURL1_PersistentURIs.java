42f977181dc474742fac5bafb64b73ac81050282
Daniel Garijo <dgarijov@gmail.com>
2025-07-31 10:43:18 +0200

    Fix #56

--- src/main/java/entities/checks/Check_PURL1_PersistentURIs.java
+++ src/main/java/entities/checks/Check_PURL1_PersistentURIs.java
@@ -42,6 +42,8 @@ public class Check_PURL1_PersistentURIs extends Check {
                 (this.ontology_URI.contains("purl.") && this.ontology_URI.contains(".org")) ||
                 // see https://www.linked.data.gov.au/governance
                 this.ontology_URI.contains("linked.data.gov.au") ||
+                this.ontology_URI.contains("dbpedia.org") ||
+                this.ontology_URI.contains("perma.cc") ||
                 this.ontology_URI.contains("www.w3.org")){
             this.status = Constants.OK;
             this.explanation = Constants.PURL1_EXPLANATION_OK;
