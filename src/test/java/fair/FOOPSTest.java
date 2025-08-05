46e724363c7cf78c443282fd9d97d993131ed0bb
Daniel Garijo <dgarijov@gmail.com>
2025-08-05 15:44:58 +0200

    add tests

--- src/test/java/fair/FOOPSTest.java
+++ src/test/java/fair/FOOPSTest.java
@@ -1,6 +1,7 @@
 package fair;
 
 import entities.Ontology;
+import entities.checks.Check_PURL1_PersistentURIs;
 import org.junit.Test;
 
 import java.io.File;
@@ -12,7 +13,8 @@ public class FOOPSTest {
 
     /**
      * This test verifies that the 61 concepts in the conceptual_mapping vocabulary are documented.
-     * The test uses skos for an owl ontology
+     * The test uses skos for descriptions.
+     * One of the labels uses obo:0000118 (alt label)
      */
     @Test
     public void skosAndRdfsAnnotationsUsedForOntology(){
@@ -20,6 +22,7 @@ public class FOOPSTest {
         File is = new File(classLoader.getResource("conceptual_mapping.ttl").getFile());
         FOOPS f = new FOOPS(is.toString(), true);
         assertEquals (61,f.getOntology().getTermsWithDescription().size());
+        assertEquals (61,f.getOntology().getTermsWithLabel().size());
         f.removeTemporaryFolders();
     }
 
@@ -34,5 +37,21 @@ public class FOOPSTest {
         // there must be one author
         assertEquals (1,f.getOntology().getAuthors().size());
         f.removeTemporaryFolders();
+    }
+
+    /**
+     * This test verifies if an ontology with uri purl.something.org passes as persistent
+     */
+    @Test
+    public void persistentURI(){
+        ClassLoader classLoader = getClass().getClassLoader();
+        File is = new File(classLoader.getResource("test_persistent_uri.ttl").getFile());
+        FOOPS f = new FOOPS(is.toString(), true);
+        Check_PURL1_PersistentURIs check = new Check_PURL1_PersistentURIs(f.getOntology());
+        check.check();
+        // URI should be persistent
+        assertEquals(Constants.OK, check.getStatus());
+        f.removeTemporaryFolders();
+
     }
 }
